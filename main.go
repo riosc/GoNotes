a4272728b277eea9eb2a64cb4abb57610c283a4f
Carlos David Rios Vertel <rioscarlosd@gmail.com>
2018-09-20 06:10:58 -0300

    1-4-exercise

--- main.go
+++ main.go
@@ -2,50 +2,67 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
 )
 
+/*
+
+
+//dup V2
 func main() {
-	/*
-		var s, sep string
-		for i := 1; i < len(os.Args); i++ {
-			s += sep + os.Args[i]
-			sep = "/"
+	counts := make(map[string]int)
+	files := os.Args[1:]
+
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, error := os.Open(arg)
+			if error != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", error)
+				continue
+			}
+
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
+*/
+
+func main() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 		}
 
-		fmt.Println(s)
-	*/
-
-	/*
-		i := len(os.Args)
-		for i >= 0 {
-			fmt.Println(i)
-			i--
-		}
-	*/
-
-	/*
-		s, sep := "", ""
-		for index, arg := range os.Args[1:] {
-			s += sep + strconv.FormatInt(int64(index), 10) + arg
-			sep = "/"
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
 		}
 
-		fmt.Println(s)
-	*/
+		for line, n := range counts {
+			if n > 1 {
+				fmt.Printf("%d\t%s\t%s\n", n, line, filename)
+			}
+		}
 
-	fmt.Println(strings.Join(os.Args[1:], "/"))
-	fmt.Println(os.Args[1:])
-
-	/// Three forms of variable declarations
-	/*
-		s := "" // Short declaration, only inside functions, not for package level variables **
-		var s = "" // Rarely used, except when declaring multiple variables
-		var s string // Relies on default initialization **
-		var s string = "" // Necesary when variable type isn't the same type than the initialization value
-
-		/// ** : most used in practice
-	*/
-
+	}
 }
