62c7bdbc60966452979464819ae6fd6ceb6aaba0
Carlos David Rios Vertel <rioscarlosd@gmail.com>
2018-09-20 07:05:31 -0300

    1-5 exercise

--- main.go
+++ main.go
@@ -1,68 +1,51 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
 	"os"
-	"strings"
 )
 
-/*
+var palette = []color.Color{color.White, color.RGBA{0x00, 0x80, 0x00, 0xFF}}
 
-
-//dup V2
-func main() {
-	counts := make(map[string]int)
-	files := os.Args[1:]
-
-	if len(files) == 0 {
-		countLines(os.Stdin, counts)
-	} else {
-		for _, arg := range files {
-			f, error := os.Open(arg)
-			if error != nil {
-				fmt.Fprintf(os.Stderr, "dup2: %v\n", error)
-				continue
-			}
-
-			countLines(f, counts)
-			f.Close()
-		}
-	}
-
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
-		}
-	}
-}
-
-func countLines(f *os.File, counts map[string]int) {
-	input := bufio.NewScanner(f)
-	for input.Scan() {
-		counts[input.Text()]++
-	}
-}
-*/
+const (
+	whiteIndex = 0
+	blackIndex = 1
+)
 
 func main() {
-	counts := make(map[string]int)
-	for _, filename := range os.Args[1:] {
-		data, err := ioutil.ReadFile(filename)
+	lissajous(os.Stdout)
+}
 
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
 		}
-
-		for _, line := range strings.Split(string(data), "\n") {
-			counts[line]++
-		}
-
-		for line, n := range counts {
-			if n > 1 {
-				fmt.Printf("%d\t%s\t%s\n", n, line, filename)
-			}
-		}
-
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
 	}
+	gif.EncodeAll(out, &anim)
 }
