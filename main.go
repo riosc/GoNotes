96fef7de9ef8ee9a8c2b28c6e7cf275a1f129370
Carlos David Rios Vertel <rioscarlosd@gmail.com>
2018-09-21 07:23:11 -0300

    1_7, 1_8, 1_9 Exercises

--- main.go
+++ main.go
@@ -1,53 +1,40 @@
 package main
 
 import (
-	"image"
-	"image/color"
-	"image/gif"
+	"fmt"
 	"io"
-	"math"
-	"math/rand"
+	"net/http"
 	"os"
+	"strings"
 )
 
-var palette = []color.Color{color.White, color.RGBA{0xFF, 0xFF, 0x00, 0xFF}, color.RGBA{0xFF, 0x00, 0x00, 0xFF}, color.RGBA{0x00, 0x80, 0x00, 0xFF}}
-
 const (
-	whiteIndex = 0
-	blackIndex = 1
+	scheme = "http://"
 )
 
 func main() {
-	lissajous(os.Stdout)
-}
+	for _, url := range os.Args[1:] {
 
-func lissajous(out io.Writer) {
-	const (
-		cycles  = 5
-		res     = 0.001
-		size    = 100
-		nframes = 64
-		delay   = 8
-	)
-
-	freq := rand.Float64() * 3.0
-	anim := gif.GIF{LoopCount: nframes}
-	phase := 0.0
-
-	for i := 0; i < nframes; i++ {
-		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
-		img := image.NewPaletted(rect, palette)
-
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
-			x := math.Sin(t)
-			y := math.Sin(t*freq + phase)
-
-			indexColor := uint8(float64(len(palette)) * rand.Float64())
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), indexColor)
+		if strings.HasPrefix(url, scheme) == false {
+			url = scheme + url
 		}
-		phase += 0.1
-		anim.Delay = append(anim.Delay, delay)
-		anim.Image = append(anim.Image, img)
+
+		resp, error := http.Get(url)
+		if error != nil {
+			fmt.Fprintf(os.Stderr, "Fetch: %v\n", error)
+			os.Exit(1)
+		}
+
+		// b, error := ioutil.ReadAll(resp.Body)
+		// resp.Body.Close()
+
+		copyResp, error := io.Copy(os.Stdout, resp.Body)
+
+		if error != nil {
+			fmt.Fprintf(os.Stderr, "fetch: copying %s: %v\n", url, error)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%s -- StatusCode: %s\n", string(copyResp), resp.Status)
 	}
-	gif.EncodeAll(out, &anim)
 }
